3ce6a66ff291a81131de84ca6046f2b6cbd5112a
lancerace <lancerace144@gmail.com>
2022-05-08 19:49:38 +0800

    up

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,12 @@ const server = express()
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }))
 server.use(cors());
-
+server.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 server.listen(process.env.PORT || 4200, async () => {
     logger.info(`Server started at PORT ${process.env.PORT} in ${process.env.NODE_ENV}`);
 
