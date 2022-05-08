134c7173fabddd5d7b71b30399ad4db9e6afa299
lancerace <lancerace144@gmail.com>
2022-05-08 20:07:40 +0800

    up

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,38 @@
 require('dotenv').config();
 import "reflect-metadata";//typeorm 
 import cors from 'cors';
+import {urlencoded, json} from 'body-parser';
 import express from 'express';
 import logger from './utils/logger';
 import MainController from "./controllers";
 import mongoose from 'mongoose';
 
 const server = express()
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }))
-server.use(cors());
-server.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+server.use(json());
+server.use(urlencoded({ extended: true }))
+//server.use(cors());
+
+// Add headers before the routes are defined
+server.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
     next();
 });
+
+
 server.listen(process.env.PORT || 4200, async () => {
     logger.info(`Server started at PORT ${process.env.PORT} in ${process.env.NODE_ENV}`);
 
